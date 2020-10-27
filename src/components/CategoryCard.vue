0297a36898eb4bb26f061a3dc02911309aebb920
mayallzObject <mayallzobject@gmail.com>
2020-10-27 16:11:19 +0100

    styling

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="meal-card">
-    <p>{{ cat.strCategory }}</p>
+  <div class="category-card">
+    <img class="img" :src="cat.strCategoryThumb" alt="Image" />
+    <p>{{ cat.strCategoryDescription }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
-import { mapState } from "vuex"
 
 export default Vue.extend({
   name: "CategoryCard",
   props: {
     cat: Object
-  },
-  computed: mapState(["meals"])
+  }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.category-card {
+  padding: 20px;
+  margin-bottom: 20px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.category-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.category-link {
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+}
+.img {
+  width: 170px;
+  height: 170px;
+}
+</style>
