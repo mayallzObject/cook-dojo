e0912d413adb7540b5fdf3a099c78345ad12c99b
mayallzObject <mayallzobject@gmail.com>
2020-10-27 18:49:07 +0100

    category buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="home">
-    <MealCard v-for="meal in meals" :key="meal.id" :meal="meal" />
+  <div>
+    <div>
+      <CatButton v-for="cat in categories" :key="cat.id" :cat="cat" />
+    </div>
+    <div class="home">
+      <MealCard v-for="meal in meals" :key="meal.id" :meal="meal" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
 import MealCard from "@/components/MealCard.vue"
+import CatButton from "@/components/CatButton.vue"
 import { mapState } from "vuex"
 
 export default Vue.extend({
   name: "Home",
   components: {
-    MealCard
+    MealCard,
+    CatButton
   },
   created() {
     this.$store.dispatch("fetchMeals", {})
+    this.$store.dispatch("fetchCategories", {})
   },
-  computed: mapState(["meals"])
+  computed: mapState(["meals", "categories"])
 })
 </script>
