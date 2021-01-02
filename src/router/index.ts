894d9c869ef334c313945d15df9b7a836f77bb5c
mayallzObject <mayallzobject@gmail.com>
2021-01-03 00:55:34 +0100

    testing

--- src/router/index.ts
+++ src/router/index.ts
@@ -45,7 +45,7 @@ const routes: Array<RouteConfig> = [
 ]
 
 const router = new VueRouter({
-  // mode: "history", // <----
+  mode: "history", // <----
   routes,
   base: process.env.BASE_URL,
   scrollBehavior(to, from, savedPosition) {
